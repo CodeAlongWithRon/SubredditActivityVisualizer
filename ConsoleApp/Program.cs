48084298acd51991138caff56f7815c3b00b2839
Ron <codealongwithron.github.io>
2020-10-18 23:11:26 +0200

    Part 1

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SubredditActivityVisualizer.ConsoleApp
+{
+   class Program
+   {
+      static async Task Main(string[] args)
+      {
+         using (var httpClient = new HttpClient())
+         {
+            var subreddit = Console.ReadLine();
+            var httpResponseMessage = await httpClient.GetAsync($"https://www.reddit.com/r/{subreddit}/about.json");
+            var responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+            var subredditAboutResponse = JsonConvert.DeserializeObject<SubredditAboutResponse>(responseContent);
+
+            Console.WriteLine(subredditAboutResponse.Data.Subscribers);
+         }
+      }
+   }
+}
