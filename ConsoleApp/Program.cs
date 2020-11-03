7f840ed3ef127d3078f6b91e6a711938b845eb94
Ron <codealongwithron.github.io>
2020-11-03 22:55:52 +0100

    Implemented di for console application

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SubredditActivityVisualizer.Application;
 using SubredditActivityVisualizer.Application.Services;
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace SubredditActivityVisualizer.ConsoleApp
@@ -10,11 +10,17 @@ namespace SubredditActivityVisualizer.ConsoleApp
    {
       static async Task Main(string[] args)
       {
-         //var subreddit = Console.ReadLine();
-         //var service = new GetSubscribersService();
-         //var subscribers = await service.GetAsync(subreddit);
+         var serviceCollection = new ServiceCollection();
+         DependencyRegistration.RegisterDependencies(serviceCollection);
 
-         //Console.WriteLine(subscribers);
+         using (var serviceProvider = serviceCollection.BuildServiceProvider())
+         {
+            var subreddit = Console.ReadLine();
+            var service = serviceProvider.GetService<IGetSubscribersService>();
+            var subscribers = await service.GetAsync(subreddit);
+
+            Console.WriteLine(subscribers);
+         }
       }
    }
 }
